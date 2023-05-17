f64f00fb415f781c388de07a1bc0c5a9b9c34c33
Edy Galdiolo <edygaldy302@gmail.com>
2023-05-18 01:29:26 +0200

    feat(js): random generator of mail

--- js/script.js
+++ js/script.js
@@ -3,9 +3,18 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            mailAPI: 'https://flynn.boolean.careers/exercises/api/random/mail'
+            mailAPI: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            responseMail: []
         }
     },
     methods: {
+    },
+    mounted() {
+        for (let i=0; i < 10; i++){
+            axios.get(this.mailAPI).then((mail) => {
+                let tempMail = mail.data.response;
+                this.responseMail.push(tempMail);
+            });
+        }
     }
 }).mount('#app')
